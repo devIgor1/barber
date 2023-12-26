4ce3c8e81e20c9936efc123731b1778e993cb826
Igor <igormraesmm@gmail.com>
2023-12-26 16:19:27 -0300

    feat: Add types of cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response, NextFunction } from "express"
 
+import "express-async-errors"
+
 import { router } from "./routes"
 
 const app = express()
@@ -7,6 +9,19 @@ const app = express()
 app.use(express.json())
 
 app.use(router)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message,
+    })
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+  })
+})
 
 app.listen(3456, () => {
   console.log("SERVER ONLINE")
