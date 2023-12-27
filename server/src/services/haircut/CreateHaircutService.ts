26dd65ed2e036422d6679bf67cafdfcc74995882
Igor <igormraesmm@gmail.com>
2023-12-27 12:36:59 -0300

    feat: Create premium limit
    	new file:   src/controllers/haircut/ListUserHaircutsController.ts
        new file:   src/services/haircut/ListUserHaircutsService.ts

--- server/src/services/haircut/CreateHaircutService.ts
+++ server/src/services/haircut/CreateHaircutService.ts
@@ -12,6 +12,31 @@ class CreateHaircutService {
       throw new Error("Please fill in all fields.")
     }
 
+    // Verify how many haircut models this user has
+
+    const myHaircuts = await prisma.haircut.count({
+      where: {
+        userId: userId,
+      },
+    })
+
+    // Verify if the user is premium or not, limiting the number of haircut models available to create
+
+    const user = await prisma.user.findFirst({
+      where: {
+        id: userId,
+      },
+      include: {
+        subscriptions: true,
+      },
+    })
+
+    // Creating the limitation of haircut models
+
+    if (myHaircuts >= 3 && user?.subscriptions?.status !== "ACTIVE") {
+      throw new Error("Not Authorized")
+    }
+
     const haircut = await prisma.haircut.create({
       data: {
         name: name,
