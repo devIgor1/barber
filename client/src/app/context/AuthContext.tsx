a3d267d60e24bc2f2bc22755dec54b3e094c942e
Igor <igormraesmm@gmail.com>
2023-12-28 15:37:55 -0300

    feat: Initial api setup

--- client/src/app/context/AuthContext.tsx
+++ client/src/app/context/AuthContext.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import { createContext, ReactNode, useState } from "react"
+import { destroyCookie } from "nookies"
+import Router from "next/router"
 
 interface AuthContextData {
   user: UserProps | undefined
@@ -28,6 +30,15 @@ interface SignInProps {
 }
 
 export const AuthContext = createContext({} as AuthContextData)
+
+export function signOut() {
+  try {
+    destroyCookie(null, "@barber.token", { path: "/" })
+    Router.push("/sign-in")
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 export function AuthProvider({ children }: { children: ReactNode }) {
   const [user, setUser] = useState<UserProps>()
