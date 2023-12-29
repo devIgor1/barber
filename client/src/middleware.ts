5fc26a03690a919d3dc7dca4e0ba9c69837bd6c7
Igor <igormraesmm@gmail.com>
2023-12-29 17:26:01 -0300

    feat: Add middleware to protect routes

---
+++ client/src/middleware.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server"
+
+export default function middleware(req: NextRequest) {
+  const token = req.cookies.get("@barber.token")?.value
+
+  const signInURL = new URL("/sign-in", req.url)
+  const dashboardURL = new URL("/dashboard", req.url)
+
+  if (!token) {
+    if (
+      req.nextUrl.pathname === "/sign-in" ||
+      req.nextUrl.pathname === "/sign-up"
+    ) {
+      return NextResponse.next()
+    } else {
+      req.nextUrl.pathname === "/dashboard"
+    }
+    return NextResponse.redirect(signInURL)
+  }
+
+  if (token) {
+    if (
+      req.nextUrl.pathname === "/sign-in" ||
+      req.nextUrl.pathname === "/sign-up"
+    ) {
+      return NextResponse.redirect(dashboardURL)
+    }
+  }
+}
+
+export const config = {
+  matcher: ["/sign-in", "/sign-up", "/dashboard/:path"],
+}
