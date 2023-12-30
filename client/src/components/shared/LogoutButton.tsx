8d40a6ed120d97ca8d5492125e0d76598b41c71c
Igor <igormraesmm@gmail.com>
2023-12-30 16:29:10 -0300

    feat: Add className prop

--- client/src/components/shared/LogoutButton.tsx
+++ client/src/components/shared/LogoutButton.tsx
@@ -4,14 +4,15 @@ import { AuthContext } from "@/app/context/AuthContext"
 import { useContext } from "react"
 import { CiLogout } from "react-icons/ci"
 
-const LogoutButton = () => {
+interface LogoutButtonProps {
+  className?: string
+}
+
+const LogoutButton = ({ className }: LogoutButtonProps) => {
   const { logoutUser } = useContext(AuthContext)
 
   return (
-    <button
-      onClick={logoutUser}
-      className="flex-center gap-3 border-2 border-white p-2 rounded-lg hover:scale-95 duration-300"
-    >
+    <button onClick={logoutUser} className={className}>
       <h1>Logout</h1>
       <CiLogout size={30} />
     </button>
