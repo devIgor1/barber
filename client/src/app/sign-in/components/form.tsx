e7ff9c37a66781e165cb8b8b65cc37a8b799e26f
Igor <igormraesmm@gmail.com>
2023-12-30 14:08:01 -0300

    refactor: Change Logo folder

--- client/src/app/sign-in/components/form.tsx
+++ client/src/app/sign-in/components/form.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import Logo from "@/components/Logo"
+import Logo from "@/components/shared/Logo"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
