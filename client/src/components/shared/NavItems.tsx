a869e13c8f1c920d8382a9ab517988121cd74760
Igor <igormraesmm@gmail.com>
2023-12-30 14:00:51 -0300

    feat: Add className as prop

--- client/src/components/shared/NavItems.tsx
+++ client/src/components/shared/NavItems.tsx
@@ -1,18 +1,18 @@
 import navLinks, { NavLinksProps } from "@/constants"
 import Link from "next/link"
 
-const NavItems = () => {
+interface NavItemsProps {
+  className?: string
+}
+
+const NavItems = ({ className }: NavItemsProps) => {
   return (
     <ul className="flex-center w-full flex-col gap-10 mt-14">
       {navLinks.map((link: NavLinksProps) => (
-        <Link
-          href={link.route}
-          className="text-xl flex-center w-full hover:bg-zinc-800 duration-300 p-4"
-          key={link.route}
-        >
+        <Link href={link.route} className={className} key={link.route}>
           {link.icon({ className: "mr-2" })}
           {/* Here I'm rendering the icon directly*/}
-          <li className="">{link.name}</li>
+          <li>{link.name}</li>
         </Link>
       ))}
     </ul>
