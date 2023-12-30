4fb5a0172b9bd8cc2d72373a31bc3b5349940f59
Igor <igormraesmm@gmail.com>
2023-12-30 16:46:21 -0300

    refactor: Remove unnecessary imports

--- client/src/components/shared/MobileNav.tsx
+++ client/src/components/shared/MobileNav.tsx
@@ -1,11 +1,4 @@
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import { FiMenu } from "react-icons/fi"
 import { Separator } from "../ui/separator"
 import Link from "next/link"
