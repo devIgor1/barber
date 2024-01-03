fed392a1473fb2cb0722774c8ff4e3f3b09f01e2
Igor <igormraesmm@gmail.com>
2024-01-03 08:56:20 -0300

    fix: Fix height of screen

--- client/src/app/dashboard/layout.tsx
+++ client/src/app/dashboard/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="flex h-screen">
+    <div className="flex min-h-screen">
       <Sidebar />
       <main className="flex-1 bg-zinc-950">{children}</main>
     </div>
